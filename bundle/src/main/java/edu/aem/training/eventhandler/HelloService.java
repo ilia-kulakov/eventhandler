0b231ebad062a24e87f4bbba941a4b2cf0db49c0
veterkam <veterkam@gmail.com>
2019-01-30 11:37:53 +0300

    Initial commit

---
+++ bundle/src/main/java/edu/aem/training/eventhandler/HelloService.java
@@ -0,0 +1,13 @@
+package edu.aem.training.eventhandler;
+
+/**
+ * A simple service interface
+ */
+public interface HelloService {
+    
+    /**
+     * @return the name of the underlying JCR repository implementation
+     */
+    public String getRepositoryName();
+
+}
